40c35f88c41d5a77406c259de5ab1ab5212ca99f
leeyimin <imleeyimin@gmail.com>
2016-09-28 02:47:07 +0800

    Add execute body for EditCommand

--- src/seedu/addressbook/commands/EditCommand.java
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -1,11 +1,14 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.exception.IllegalValueException;
 import seedu.addressbook.data.person.Address;
 import seedu.addressbook.data.person.Email;
 import seedu.addressbook.data.person.Name;
 import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
 
 public class EditCommand extends Command {
     
@@ -47,8 +50,16 @@ public class EditCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            final Person target = getWriteablePerson();
+            addressBook.removePerson(target);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, target));
+
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (PersonNotFoundException pnfe) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+        }
     }
 
 }
